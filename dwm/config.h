c675a9ecde67f59d4fa6b626e445cfaceb4cf5e8
OtoyaLee <zombie11019@outlook.com>
2023-02-11 20:11:36 +0800

    modify shortkey and icons

--- dwm/config.h
+++ dwm/config.h
@@ -5,7 +5,9 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=13",
+					"WenQuanYi Micro Hei:size=13:type=Regular:antialias=true:autohint=true",
+					"Symbols Nerd Font:pixelsize=16:type=2048-em:antialias=true:autohint=true"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -26,7 +28,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "","", "󰨞", "󰓇", "󰘅", "󰉋", "󰟀"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -52,7 +54,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -65,12 +67,26 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *volumedown[]  = { "/home/otoya/SoftWare/dwm/volumedown.sh", NULL };
+static const char *volumeup[]  = { "/home/otoya/SoftWare/dwm/volumeup.sh", NULL };
+static const char *lightdown[]  = { "/home/otoya/SoftWare/dwm/lightdown.sh", NULL };
+static const char *lightup[]  = { "/home/otoya/SoftWare/dwm/lightup.sh", NULL };
+static const char *rofi[]  = { "/home/otoya/SoftWare/dwm/rofi.sh", NULL };
+static const char *i3lock[]  = { "/home/otoya/SoftWare/dwm/i3lock.sh", NULL };
+static const char *shotcut[]  = { "/home/otoya/SoftWare/dwm/shotcut.sh", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_F2,     spawn,          {.v = volumedown } },
+	{ MODKEY,                       XK_F3,     spawn,          {.v = volumeup } },
+	{ MODKEY|ShiftMask,             XK_s,     spawn,           {.v = shotcut } },
+	{ MODKEY,                       XK_q,     spawn,           {.v = rofi } },
+	{ MODKEY|ShiftMask,             XK_l,     spawn,           {.v = i3lock } },
+	{ MODKEY,                       XK_F5,     spawn,          {.v = lightdown } },
+	{ MODKEY,                       XK_F6,     spawn,          {.v = lightup } },
+	{ MODKEY,             			XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -78,7 +94,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
